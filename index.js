d14b8b27019e5b07527d67837f48e41e9361b38e
Nicholas Werner <micropacman@gmail.com>
2018-09-14 16:01:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ function nowServing(katzDeliLine) {
   return `Currently serving ${firstPersonInLine}.`
 }
 
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  }
+}
